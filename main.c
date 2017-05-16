ecccece708f12190eeceed6a9a67f5d3026f8630
Spacket <valerij.stelmach@live.de>
2017-05-16 15:02:07 +0200

    richtige c datei

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+    int data[3];
+
+    struct daten {
+        char key;
+        char value;
+    };
+}
+
+int put(char* key, char* value){
+    struct daten d;
+    d.key = key;
+    d.value = value;
 }
